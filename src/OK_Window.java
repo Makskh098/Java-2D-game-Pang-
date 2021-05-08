ded181bc9415506c1e885ecaf4bdb0707fe1b8c0
KotletJB <bronowski.jan2@gmail.com>
2021-05-09 00:43:27 +0200

    Implementing main menu

---
+++ src/OK_Window.java
@@ -0,0 +1,64 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+
+public class OK_Window extends JFrame implements ActionListener{
+
+    public String title;
+    public String text;
+
+
+    OK_Window(String title, String text){
+        this.title = title;
+        this.text = text;
+    }
+
+    public void window() {
+        setTitle(title);
+
+        //Ok button
+        JPanel box_buttons = new JPanel(new GridLayout(1, 1));
+
+        JButton ok_b = new JButton("Ok");
+        ok_b.addActionListener(this);
+        ok_b.setActionCommand("click");
+
+        box_buttons.add(ok_b);
+
+        JPanel south = new JPanel(new GridBagLayout());
+        south.add(box_buttons);
+
+
+        //text
+        JLabel text_to_write = new JLabel("<html><div style='text-algin: center;'>" + text +  "</div></html" , SwingConstants.CENTER);
+        JScrollPane scroll = new JScrollPane(text_to_write);
+
+
+        //content
+        getContentPane().add(south, BorderLayout.SOUTH);
+        getContentPane().add(scroll);
+
+
+    }
+        // event
+        @Override
+        public void actionPerformed(ActionEvent ae) {
+            String action = ae.getActionCommand();
+            if (action.equals("click")) {
+                dispatchEvent(new WindowEvent(this, WindowEvent.WINDOW_CLOSING));
+            }
+        }
+
+        public void createAndShowGUI() {
+
+        window();
+
+        this.pack();
+        this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        this.setVisible(true);
+
+
+    }
+}
--- src/Welcome_Screen.java
+++ src/Welcome_Screen.java
@@ -1,7 +1,10 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
 
-public class Welcome_Screen extends JFrame {
+public class Welcome_Screen extends JFrame implements ActionListener {
     private JButton start_b, table_b, autors_b, exit_b;
     private JLabel center;
     private JScrollPane scroll;
@@ -9,24 +12,35 @@ public class Welcome_Screen extends JFrame {
     private String welcome_text;
     public void window(){
 
+        setBounds(0, 0,400,400);
         setTitle("PANG");
 
+
+        // buttons
         start_b = new JButton("Start");
         table_b = new JButton("Leader Board");
-        autors_b = new JButton("Autors");
+        autors_b = new JButton("Credits");
         exit_b = new JButton("Exit");
 
+
+        // buttons actions
+        autors_b.addActionListener(this);
+        autors_b.setActionCommand("credits");
+
+        exit_b.addActionListener(this);
+        exit_b.setActionCommand("exit");
+
+        // buttons layout
         box = new JPanel(new GridLayout(4,1));
         box.add(start_b);
         box.add(table_b);
         box.add(autors_b);
         box.add(exit_b);
 
-
-
         JPanel south = new JPanel(new GridBagLayout());
         south.add(box);
 
+        //text
         welcome_text = "MENU" +
                 "<br>" +
                 "max to dobry ziom";
@@ -34,20 +48,33 @@ public class Welcome_Screen extends JFrame {
 
 
         center = new JLabel(text,SwingConstants.CENTER);
-
         scroll = new JScrollPane(center);
 
+        //content
         getContentPane().add(south, BorderLayout.SOUTH);
         getContentPane().add(scroll);
 
-
+        //display
         this.pack();
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setVisible(true);
 
+    }
 
+    //events
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+        String action = ae.getActionCommand();
 
+        if (action.equals("credits")) {
+            OK_Window credits = new OK_Window("Credits", "Jan Bronowski" + "<br>" + "Maks K.");
+            credits.createAndShowGUI();
+        }
 
+        if (action.equals("exit")) {
+            dispatchEvent(new WindowEvent(this, WindowEvent.WINDOW_CLOSING));
+        }
 
     }
+
 }
