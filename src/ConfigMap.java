885e760433c6aef2961dfb3c219970b36ba3f0a4
Maksym Pawel Khachapuridze <m.khachapuridze@stud.elka.pw.edu.pl>
2021-05-17 22:59:40 +0200

    added some comments

--- src/ConfigMap.java
+++ src/ConfigMap.java
@@ -7,7 +7,20 @@ public class ConfigMap {
     float percent_of_medium_balls;
     float percent_of_large_balls;
     float percent_of_extraLarge_balls;
+/***
+ * ConfigMap this class stores properties of maps data
+ */
 
+    /***
+     * Constructor of ConfigMap Class
+     * @param length_of_map length of map
+     * @param height_of_map height of map
+     * @param number_of_balls number of balls
+     * @param percent_of_small_balls percent of small balls that will appear on map defined for every stage
+     * @param percent_of_medium_balls percent of medium balls that will appear on map defined for every stage
+     * @param percent_of_large_balls percent of large balls that will appear on map defined for every stage
+     * @param percent_of_extraLarge_balls percent of extra Large balls that will appear on map defined for every stage
+     */
     ConfigMap(double length_of_map, double height_of_map, int number_of_balls, float percent_of_small_balls, float percent_of_medium_balls, float percent_of_large_balls, float percent_of_extraLarge_balls){
         this.length_of_map = length_of_map;
         this.height_of_map = height_of_map;
@@ -20,6 +33,10 @@ public class ConfigMap {
 
     }
 
+    /***
+     * Method provided to return string version of Configuration data of map
+     * @return returns string with information of every parameter
+     */
     @Override
     public String toString() {
         return "{" +
